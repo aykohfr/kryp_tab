b3870093b0215fe5fdb5fef5f6a676a8c7ba3267
Aykoh <biscop70@gmail.com>
2026-08-31 21:27:32 +0200

    Supprime le chargement de l'ancien correctif UI

--- lua/autorun/kryp_tab.lua
+++ lua/autorun/kryp_tab.lua
@@ -3,12 +3,10 @@ KrypTab = KrypTab or {}
 if SERVER then
     AddCSLuaFile("kryp_tab/sh_config.lua")
     AddCSLuaFile("kryp_tab/cl_tablist.lua")
-    AddCSLuaFile("kryp_tab/cl_layoutfix.lua")
 end
 
 include("kryp_tab/sh_config.lua")
 
 if CLIENT then
     include("kryp_tab/cl_tablist.lua")
-    include("kryp_tab/cl_layoutfix.lua")
 end
