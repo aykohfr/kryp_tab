5c278a9ec2a45394c23446557b8e82edbd25bdd7
Aykoh <biscop70@gmail.com>
2026-08-31 21:20:01 +0200

    Charge les corrections de layout du tablist

--- lua/autorun/kryp_tab.lua
+++ lua/autorun/kryp_tab.lua
@@ -3,10 +3,12 @@ KrypTab = KrypTab or {}
 if SERVER then
     AddCSLuaFile("kryp_tab/sh_config.lua")
     AddCSLuaFile("kryp_tab/cl_tablist.lua")
+    AddCSLuaFile("kryp_tab/cl_layoutfix.lua")
 end
 
 include("kryp_tab/sh_config.lua")
 
 if CLIENT then
     include("kryp_tab/cl_tablist.lua")
+    include("kryp_tab/cl_layoutfix.lua")
 end
